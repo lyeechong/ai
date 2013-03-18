a3e777e4b1e61f9a59e9e724383523cee6172aee
shen <shen@tora.(none)>
2013-03-18 02:16:21 -0500

    think everything up to 3b is done (waiting on Piazza to clarify cliff risking and noise stuff for 3a and 3b though)

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -19,16 +19,16 @@ def question2():
   return answerDiscount, answerNoise
 
 def question3a():
-  answerDiscount = 0.9
-  answerNoise = 0.2
+  answerDiscount = 0.3
+  answerNoise = 0.0
   answerLivingReward = 0.0
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerDiscount = 0.02
+  answerNoise = 0.1
+  answerLivingReward = -0.1
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
