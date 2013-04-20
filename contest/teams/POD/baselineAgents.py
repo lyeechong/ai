e59720d566a26269325aa6c5945a3e68b402e803
shen <shen@tora.(none)>
2013-04-20 00:33:24 -0500

    agents work well GO TEAM

--- contest/teams/POD/baselineAgents.py
+++ contest/teams/POD/baselineAgents.py
@@ -25,7 +25,9 @@ class BaselineAgents(AgentFactory):
 
   def __init__(self, isRed, first='offense', second='defense', rest='offense'):
     AgentFactory.__init__(self, isRed)
-    self.agents = [first, second]
+    #self.agents = [first, second]
+    # -- change to all offensive agents since I haven't worked on the defensive yet
+    self.agents = ['offense', 'offense']
     self.rest = rest
     self.isRed = isRed
 
@@ -146,16 +148,133 @@ class OffensiveReflexAgent(ReflexCaptureAgent):
     successor = self.getSuccessor(gameState, action)
     features['successorScore'] = self.getScore(successor)
 
+    currentPosition = gameState.getAgentState(self.index).getPosition()
+    currentX = currentPosition[0]
+    currentY = currentPosition[1]
+
+    # -- Note: call the isAPacman thing on the successor since you can change
+    # --       when going across the border
+    successor = self.getSuccessor(gameState, action)
+    isAPacman = successor.getAgentState(self.index).isPacman
+    isAGhost = not isAPacman # -- redundant?
+
+    ######################################################
+    # -- The idea behind the agent is
+    # -- to try a divide and conquer 
+    # -- approach since otherwise
+    # -- the two agents
+    # -- bunch up in the same area, which is inefficient
+    ######################################################
+    
+    # -- this is the dividing up the enemy zone part
+    sector = self.index / 2
+
+    # -- insert informertial here: "THERE'S GOTTA BE A BETTER WAY TO DO THIS!"
+    # -- probably could use getSuccessor, but I'm too lazy to rewrite the code
+    if action is Directions.STOP:
+      movementVector = (0,0)
+    elif action is Directions.NORTH:
+      movementVector = (0,1)
+    elif action is Directions.EAST:
+      movementVector = (1,0)
+    elif action is Directions.SOUTH:
+      movementVector = (0,-1)
+    elif action is Directions.WEST:
+      movementVector = (-1,0)
+    
+    
+    xMoveToSuccessor, yMoveToSuccessor = movementVector
+    successorXPos = currentX + xMoveToSuccessor
+    successorYPos = currentY + yMoveToSuccessor
+    successorPos = (successorXPos, successorYPos)
+    
+    # -- their food code, modified so the agents target the food in
+    # -- the sector they are responsible for
     # Compute distance to the nearest food
     foodList = self.getFood(successor).asList()
     if len(foodList) > 0: # This should always be True,  but better safe than sorry
       myPos = successor.getAgentState(self.index).getPosition()
-      minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
-      features['distanceToFood'] = minDistance
+      
+      # -- find the closest piece of food in the agent's sector
+      smallestOverall = 1000000
+      smallestInSector = 1000000
+      for foodPosition in foodList:
+        distanceToFoodPiece = self.getMazeDistance(successorPos, foodPosition)
+        # -- check to see if it's in the agent's designated sector
+        foodY = foodPosition[1]
+        heightOfLayout = gameState.getWalls().height
+        
+        # -- this is a constant right now... may want to change
+        numFriendlies = 2
+        
+        sectorTopY = (sector + 1) * heightOfLayout/numFriendlies
+        sectorBottomY = sector * heightOfLayout/numFriendlies
+        if foodY > sectorBottomY and foodY < sectorTopY:
+          smallestInSector = min(distanceToFoodPiece, smallestInSector)
+        smallestOverall = min(distanceToFoodPiece, smallestOverall)
+      
+      # -- checking if there was no food left in the agent's sector
+      if smallestInSector is 1000000:
+        smallestInSector = smallestOverall
+      
+      features['distanceToFood'] = smallestInSector
+    
+    # -- capsule checking
+    # -- right now does not take into account if there are ghosts nearby or not
+    for capsuleXPos, capsuleYPos in gameState.getCapsules():
+      if successorXPos is capsuleXPos and successorYPos is capsuleYPos and isPacman:
+        features['capsuleEaten'] = 1.0
+
+    # -- Ghost checking stuff, how close they are and if they're scared
+    #currentObsGameState = getCurrentObservation()
+    # -- using gameState instead of current observation... I think they're the same
+    for enemyAgentIndex in self.getOpponents(gameState):
+      enemyAgent = gameState.getAgentState(enemyAgentIndex)
+      enemyPosition = enemyAgent.getPosition()
+      enemyScaredTimer = enemyAgent.scaredTimer
+      
+      if enemyPosition is None:
+        # -- not sure, but this happens often
+        # -- probably due to the noisy distances!
+        # -- we definitely don't want None positions
+        continue;
+      
+      # -- check if the enemy is on top of us!
+      if self.getMazeDistance(enemyPosition, successorPos) is 0:  
+        # -- if they are, see if they're scared
+        if enemyScaredTimer > 0:
+          # -- they're harmless...
+          # -- we get no points for eating them, so leave them be
+          features['scaredEnemies1PosAway'] += 1
+        else:
+          # -- they're dangerous!
+          features['lethalEnemies1PosAway'] += 1
+      elif self.getMazeDistance(enemyPosition, successorPos) is 1:
+        # -- if they are, see if they're scared
+        if enemyScaredTimer > 0:
+          # -- they're harmless...
+          # -- we get no points for eating them, so leave them be
+          features['scaredEnemies2PosAway'] += 1
+        else:
+          # -- they're dangerous!
+          features['lethalEnemies2PosAway'] += 1
+          
+      # -- right now I'm not going to bother to check if they are more than 1 spot
+      # -- away since we have shown that pacman is awesome at dodging one ghost
+      # -- and if they use two ghosts.. they can't kill both of ours at the same time!
     return features
+    
+    
 
   def getWeights(self, gameState, action):
-    return {'successorScore': 100, 'distanceToFood': -1}
+    # -- Note: call the isAPacman thing on the successor since you can change
+    # --       when going across the border
+    successor = self.getSuccessor(gameState, action)
+    isAPacman = successor.getAgentState(self.index).isPacman
+    if isAPacman:
+      return {'successorScore': 100, 'distanceToFood': -1, 'capsuleEaten': 10, 'scaredEnemies1PosAway': 1, 'lethalEnemies1PosAway':-10, 'scaredEnemies2PosAway': 0, 'lethalEnemies2PosAway':-3}
+    else:
+      return {'successorScore': 100, 'distanceToFood': -1, 'capsuleEaten': 0, 'scaredEnemies1PosAway': 100, 'lethalEnemies1PosAway':100, 'scaredEnemies2PosAway': 50, 'lethalEnemies2PosAway':50}
 
 class DefensiveReflexAgent(ReflexCaptureAgent):
   """
